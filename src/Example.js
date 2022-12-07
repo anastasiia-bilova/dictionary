5a0d68a98ea11dabf20425cae8ebcae58983fefc
Anastasiia Bilova <anastasiabilova@gmail.com>
2022-12-07 13:43:17 +0200

    Change quotes style

--- src/Example.js
+++ src/Example.js
@@ -4,7 +4,7 @@ export default function Example(props) {
   if (props.example) {
     return (
       <div className="Example">
-        <em className="opacity-75">"{props.example}"</em>
+        <em className="opacity-75">«{props.example}»</em>
       </div>
     );
   } else {
