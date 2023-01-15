22a4dd88362361ee0b299cc1fc678f5297e44c04
Anastasiia Bilova <anastasiabilova@gmail.com>
2023-01-15 14:34:08 +0200

    Add a signature with a link to GitHub at the end of the page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
       <div className="container">
         <main>
           <Dictionary defaultKeyword="memory" />
+          <div className="caption text-center m-4">
+            <a
+              href="https://github.com/anastasiia-bilova/dictionary"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div>Designed &amp; Built by Anastasiia Bilova</div>
+            </a>
+          </div>
         </main>
       </div>
     </div>
